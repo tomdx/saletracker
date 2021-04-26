a4c572c264597ab15ce996a835d1cac847f88392
tomdx <thomasle0@outlook.com>
2021-04-26 12:56:53 +1000

    Remove test file

--- backend/test.py
+++
@@ -1,8 +0,0 @@
-from db import saletrackerdb
-from scraper import Scraper
-import requests
-
-
-r = requests.get('http://localhost:5000/api/add-from-url?user_id=debug&url=https://www.woolworths.com.au/shop/productdetails/829814')
-
-print(r.text)
